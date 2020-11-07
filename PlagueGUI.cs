691869066b466a3cb0e8682fa96aead405018be1
Plague <36628963+OFWModz@users.noreply.github.com>
2020-11-07 23:21:45 +0000

    Huge Rewrite, ButtonType Choice, Default State, Etc

--- PlagueGUI.cs
+++ PlagueGUI.cs
@@ -1,15 +1,27 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+
 using UnityEngine;
 
-namespace GUIAddons
+namespace PlagueGUI
 {
     public static class PlagueGUI
     {
+        /// <summary>
+        /// The DropDownState Instance, Holding The Main MetaData For The DropDown, Along With All The Internal Data For The Buttons, IsOpen, SearchText And ScrollPos
+        /// </summary>
         private static Dictionary<DropDownData, DropDownInternals> DropDownState = new Dictionary<DropDownData, DropDownInternals>();
 
-        public static DropDownData DropDown(Rect PositionAndScale, string MainButtonText, List<KeyValuePair<string, Action>> Buttons, bool ShowSearch = true)
+        /// <summary>
+        /// Creates A GUI.DropDown Menu - Created By Plague
+        /// </summary>
+        /// <param name="PositionAndScale">The Position And Scale Of The DropDown Menu</param>
+        /// <param name="MainButtonText">The Main Button For The DropDown's Text</param>
+        /// <param name="Buttons">Your Buttons/Toggles For The DropDown, A List Of KeyValuePairs With Key Being A Tuple Of The Button Text, The ButtonType And Default Toggle State (If Toggle ButtonType) And Value Being A Delegate To Execute On Selection</param>
+        /// <param name="ShowSearch">Whether To Show The Search Bar</param>
+        /// <returns>The DropDownData Instance</returns>
+        public static DropDownData DropDown(Rect PositionAndScale, string MainButtonText, List<KeyValuePair<Tuple<string, ButtonType, bool>, Action<bool>>> Buttons, bool ShowSearch = true)
         {
             if (string.IsNullOrEmpty(MainButtonText) || Buttons == null || Buttons.Count == 0)
             {
@@ -41,7 +53,7 @@ namespace GUIAddons
 
             if (DropDownState[DropData].IsOpen && ShowSearch)
             {
-                DropDownState[DropData].SearchText = GUI.TextArea(new Rect(PositionAndScale.x, PositionAndScale.y - 25, PositionAndScale.width, PositionAndScale.height), DropDownState[DropData].SearchText);
+                DropDownState[DropData].SearchText = GUI.TextArea(new Rect(PositionAndScale.x, PositionAndScale.y + 25, PositionAndScale.width, PositionAndScale.height), DropDownState[DropData].SearchText);
             }
 
             if (GUI.Button(PositionAndScale, (DropDownState[DropData].IsOpen ? @"/\ " : @"\/ ") + MainButtonText + (DropDownState[DropData].IsOpen ? @" /\" : @" \/")))
@@ -53,39 +65,55 @@ namespace GUIAddons
             {
                 if (DropDownState[DropData].IsOpen)
                 {
-                    DropDownState[DropData].ScrollPosition = GUI.BeginScrollView(new Rect(PositionAndScale.x, PositionAndScale.y + 50, PositionAndScale.width + 25, 500), DropDownState[DropData].ScrollPosition, new Rect(0, 0, PositionAndScale.width + 25, (PositionAndScale.height * (Buttons.Count + 1)) + 25));
+                    DropDownState[DropData].ScrollPosition = GUI.BeginScrollView(new Rect(PositionAndScale.x, PositionAndScale.y + 50, PositionAndScale.width + 25, 500), DropDownState[DropData].ScrollPosition, new Rect(0, 0, PositionAndScale.width + 25, (PositionAndScale.height * (string.IsNullOrEmpty(DropDownState[DropData].SearchText) || DropDownState[DropData].SearchText == "Search" ? Buttons.Count : Buttons.Where(b => b.Key.Item1.ToLower().Contains(DropDownState[DropData].SearchText.ToLower())).ToList().Count))));
 
                     int ButtonPosMultiplier = 0;
 
-                    foreach (KeyValuePair<string, Action> Button in Buttons)
+                    foreach (KeyValuePair<Tuple<string, ButtonType, bool>, Action<bool>> Button in Buttons)
                     {
+                        if (!DropDownState[DropData].ToggleStates.ContainsKey(Button.Key.Item1))
+                        {
+                            DropDownState[DropData].ToggleStates[Button.Key.Item1] = Button.Key.Item3;
+                        }
+
+                        void MakeButton()
+                        {
+                            if (Button.Key.Item2 == ButtonType.Button)
+                            {
+                                if (GUI.Button(new Rect(0, 25 * ButtonPosMultiplier, PositionAndScale.width, PositionAndScale.height), Button.Key.Item1))
+                                {
+                                    Button.Value?.Invoke(true);
+                                }
+                            }
+                            else
+                            {
+                                if (GUI.Toggle(new Rect(0, 25 * ButtonPosMultiplier, PositionAndScale.width, PositionAndScale.height), DropDownState[DropData].ToggleStates[Button.Key.Item1], Button.Key.Item1) != DropDownState[DropData].ToggleStates[Button.Key.Item1])
+                                {
+                                    DropDownState[DropData].ToggleStates[Button.Key.Item1] = !DropDownState[DropData].ToggleStates[Button.Key.Item1];
+
+                                    Button.Value?.Invoke(DropDownState[DropData].ToggleStates[Button.Key.Item1]);
+                                }
+                            }
+                        }
+
                         if (ShowSearch)
                         {
                             if (string.IsNullOrEmpty(DropDownState[DropData].SearchText) || DropDownState[DropData].SearchText == "Search")
                             {
-                                if (GUI.Button(new Rect(0, 25 * ButtonPosMultiplier, PositionAndScale.width, PositionAndScale.height), Button.Key))
-                                {
-                                    Button.Value?.Invoke();
-                                }
+                                MakeButton();
 
                                 ButtonPosMultiplier++;
                             }
-                            else if (Button.Key.ToLower().Contains(DropDownState[DropData].SearchText.ToLower()))
+                            else if (Button.Key.Item1.ToLower().Contains(DropDownState[DropData].SearchText.ToLower()))
                             {
-                                if (GUI.Button(new Rect(0, 25 * ButtonPosMultiplier, PositionAndScale.width, PositionAndScale.height), Button.Key))
-                                {
-                                    Button.Value?.Invoke();
-                                }
+                                MakeButton();
 
                                 ButtonPosMultiplier++;
                             }
                         }
                         else
                         {
-                            if (GUI.Button(new Rect(0, 25 * ButtonPosMultiplier, PositionAndScale.width, PositionAndScale.height), Button.Key))
-                            {
-                                Button.Value?.Invoke();
-                            }
+                            MakeButton();
 
                             ButtonPosMultiplier++;
                         }
@@ -98,17 +126,33 @@ namespace GUIAddons
             return DropData;
         }
 
+        /// <summary>
+        /// Internal Data For The DropDown
+        /// </summary>
         class DropDownInternals
         {
             public bool IsOpen = false;
             public string SearchText = "Search";
             public Vector2 ScrollPosition = Vector2.zero;
+            public Dictionary<string, bool> ToggleStates = new Dictionary<string, bool>();
         }
     }
 
+    /// <summary>
+    /// MetaData For The DropDown
+    /// </summary>
     public class DropDownData
     {
         public string DropDownButtonText;
-        public List<KeyValuePair<string, Action>> ButtonsToShow;
+        public List<KeyValuePair<Tuple<string, ButtonType, bool>, Action<bool>>> ButtonsToShow;
+    }
+
+    /// <summary>
+    /// The Type Of Button Being Added To The DropDown
+    /// </summary>
+    public enum ButtonType
+    {
+        Button,
+        Toggle
     }
 }
