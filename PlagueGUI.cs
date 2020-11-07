82cf78d7c988f429300d88751383988c8949d4dc
Plague <36628963+OFWModz@users.noreply.github.com>
2020-11-07 22:03:27 +0000

    Update PlagueGUI.cs

--- PlagueGUI.cs
+++ PlagueGUI.cs
@@ -9,7 +9,7 @@ namespace GUIAddons
     {
         private static Dictionary<DropDownData, DropDownInternals> DropDownState = new Dictionary<DropDownData, DropDownInternals>();
 
-        public static DropDownData DropDown(Rect PositionAndScale, string MainButtonText, List<KeyValuePair<string, Action>> Buttons)
+        public static DropDownData DropDown(Rect PositionAndScale, string MainButtonText, List<KeyValuePair<string, Action>> Buttons, bool ShowSearch = true)
         {
             if (string.IsNullOrEmpty(MainButtonText) || Buttons == null || Buttons.Count == 0)
             {
@@ -39,7 +39,7 @@ namespace GUIAddons
                 DropDownState[DropData] = new DropDownInternals();
             }
 
-            if (DropDownState[DropData].IsOpen)
+            if (DropDownState[DropData].IsOpen && ShowSearch)
             {
                 DropDownState[DropData].SearchText = GUI.TextArea(new Rect(PositionAndScale.x, PositionAndScale.y - 25, PositionAndScale.width, PositionAndScale.height), DropDownState[DropData].SearchText);
             }
@@ -53,22 +53,34 @@ namespace GUIAddons
             {
                 if (DropDownState[DropData].IsOpen)
                 {
-                    DropDownState[DropData].ScrollPosition = GUI.BeginScrollView(new Rect(PositionAndScale.x, PositionAndScale.y + 25, PositionAndScale.width + 25, 700), DropDownState[DropData].ScrollPosition, new Rect(0, 0, PositionAndScale.width + 25, (PositionAndScale.height * (Buttons.Count + 1)) + 25));
+                    DropDownState[DropData].ScrollPosition = GUI.BeginScrollView(new Rect(PositionAndScale.x, PositionAndScale.y + 50, PositionAndScale.width + 25, 500), DropDownState[DropData].ScrollPosition, new Rect(0, 0, PositionAndScale.width + 25, (PositionAndScale.height * (Buttons.Count + 1)) + 25));
 
                     int ButtonPosMultiplier = 0;
 
                     foreach (KeyValuePair<string, Action> Button in Buttons)
                     {
-                        if (string.IsNullOrEmpty(DropDownState[DropData].SearchText) || DropDownState[DropData].SearchText == "Search")
+                        if (ShowSearch)
                         {
-                            if (GUI.Button(new Rect(0, 25 * ButtonPosMultiplier, PositionAndScale.width, PositionAndScale.height), Button.Key))
+                            if (string.IsNullOrEmpty(DropDownState[DropData].SearchText) || DropDownState[DropData].SearchText == "Search")
                             {
-                                Button.Value?.Invoke();
-                            }
+                                if (GUI.Button(new Rect(0, 25 * ButtonPosMultiplier, PositionAndScale.width, PositionAndScale.height), Button.Key))
+                                {
+                                    Button.Value?.Invoke();
+                                }
 
-                            ButtonPosMultiplier++;
+                                ButtonPosMultiplier++;
+                            }
+                            else if (Button.Key.ToLower().Contains(DropDownState[DropData].SearchText.ToLower()))
+                            {
+                                if (GUI.Button(new Rect(0, 25 * ButtonPosMultiplier, PositionAndScale.width, PositionAndScale.height), Button.Key))
+                                {
+                                    Button.Value?.Invoke();
+                                }
+
+                                ButtonPosMultiplier++;
+                            }
                         }
-                        else if (Button.Key.ToLower().Contains(DropDownState[DropData].SearchText.ToLower()))
+                        else
                         {
                             if (GUI.Button(new Rect(0, 25 * ButtonPosMultiplier, PositionAndScale.width, PositionAndScale.height), Button.Key))
                             {
@@ -76,7 +88,6 @@ namespace GUIAddons
                             }
 
                             ButtonPosMultiplier++;
-
                         }
                     }
 
